66d4d84d23dcbe01f6ad47b77c25f0d3ff4a7e56
JakubStepanek <jakub.stepanek@tul.cz>
2021-11-18 09:34:39 +0100

    WOW hvězdičky

---
+++ 05_Loops/src/forLoops/PlayingWithFor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package forLoops;
+
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ *
+ * @author kubin
+ */
+public class PlayingWithFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int n = 5;
+        System.out.println("Line ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+
+}
