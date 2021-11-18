e81c33a63aadb0d0261c80e5ecfed846439b2262
JakubStepanek <jakub.stepanek@tul.cz>
2021-11-18 13:17:22 +0100

    Konstrbatě hototo

--- 05_Loops/src/forLoops/GuessingNumberVariants.java
+++ 05_Loops/src/forLoops/GuessingNumberVariants.java
@@ -11,6 +11,45 @@ import com.sun.tools.javac.Main;
 public class GuessingNumberVariants {
     static void computerGuesses() {
         System.out.println("tady hádá pc");
+        // rozsah BinarySearch.min/max
+        // mid = (min+max)/2
+        // while (numberByUser != mid)
+        // if numberByUser < mid
+        // max = mid
+        // else if numberByUser > mid
+        // min = mid
+        // else numberByUser = mid
+        System.out.println("Mysli si číslo v rozsahu od " + BinarySearch.min + " do " + BinarySearch.max);
+        boolean guessed = false;
+
+        int max = BinarySearch.max;
+        int min = BinarySearch.min;
+
+        while (!guessed) {
+            int mid = (max + min) / 2;
+            System.out.format("Je tvoje číslo %d ? \n", mid);
+            System.out.println("1. Moje číslo je VĚTŠÍ.");
+            System.out.println("2. Moje číslo je MENŠÍ.");
+            System.out.println("3. Je to moje číslo.");
+            int choice = BinarySearch.sc.nextInt();
+            switch (choice) {
+            case 1:
+                min = mid;
+                break;
+            case 2:
+                max = mid;
+                break;
+            case 3:
+                System.out.println("Tvoje číslo je: "+mid);
+                guessed = true;
+                break;
+
+            default:
+                System.out.println("Neplatná volba!");
+                break;
+            }
+        }
+
     }
 
     static void userGuesses() {
@@ -44,7 +83,8 @@ public class GuessingNumberVariants {
     }
 
     public static void main(String[] args) {
-        System.out.println(generateRandom(10, 20));
+        // System.out.println(generateRandom(10, 20));
+        //computerGuesses();
 
     }
 }
