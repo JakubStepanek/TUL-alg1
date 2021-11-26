698e583341c1773626c0598b34219eb8d5ec0ada
JakubStepanek <jakub.stepanek@tul.cz>
2021-11-26 11:54:59 +0100

    Finally works

--- Domaci_ulohy/src/domaci_ulohy/TabulkaPozice.java
+++ Domaci_ulohy/src/domaci_ulohy/TabulkaPozice.java
@@ -9,16 +9,19 @@ import java.util.Scanner;
 
 public class TabulkaPozice {
     public static void main(String[] args) {
-        System.out.println("zadej velikost a číslo");
+        // System.out.println("zadej velikost a číslo");
         Scanner sc = new Scanner(System.in);
-        double xCount = 4;// sc.nextInt();
-        double yCount = 6;// sc.nextInt();
-        double index = 5;// sc.nextInt();
+        double xCount = sc.nextInt();
+        double yCount = sc.nextInt();
+        double index = sc.nextInt();
 
         int row = (int) Math.ceil(index / yCount);
-        int column = (int) Math.ceil(index % yCount);
-
-        System.out.println(row);
-        System.out.println(column);
+        int column = (int) (index % yCount);
+        if (column == 0) {
+            column = (int) yCount;
+        }
+        // System.out.println(row);
+        // System.out.println(column);
+        System.out.format("%d %d", row, column);
     }
 }
