83c902c323f1bd759f887caa526eae2d71095b7c
JakubStepanek <jakub.stepanek@tul.cz>
2021-11-26 15:01:15 +0100

    TODO dělitel a násobek

--- Domaci_ulohy/src/domaci_ulohy/DelitelNasobek.java
+++ Domaci_ulohy/src/domaci_ulohy/DelitelNasobek.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+package domaci_ulohy;
 
-/**
- *
- * @author kubin
- */
-public class cv21{
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // IN
+public class DelitelNasobek {
+    
+    public static void main (String[] args) {
+       
         int number = 12;
         String factor = "1";
 
@@ -30,6 +20,6 @@ public class cv21{
 
         // OUT
         System.out.println(factor);
+        
     }
-
 }
---
+++ Domaci_ulohy/src/domaci_ulohy/TabulkaPoradi.java
@@ -0,0 +1,22 @@
+/*
+*
+*
+* @author kubin
+*/
+package domaci_ulohy;
+
+import java.util.Scanner;
+
+public class TabulkaPoradi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int xPos = sc.nextInt();
+        int yPos = sc.nextInt();
+        int columnIndex = sc.nextInt();
+        int rowIndex = sc.nextInt();
+        int indexOfTable;
+        System.out.println(indexOfTable);
+    }
+
+}
