5a248a8939811ef7c9643ca4311db42d6748d8f3
JakubStepanek <jakub.stepanek@tul.cz>
2021-12-13 22:26:40 +0100

    Vánoční úloha

--- Alg_1_Project/src/App.java
+++ Alg_1_Project/src/App.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class App {
     static public Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        showMenu();
-        drawFish();
+        // showMenu();
+        // drawFish();
+
+        test();
+    }
+
+    private static void test() {
+        int N = 7;
+        drawFish(N);
     }
 
     private static void showMenu() {
@@ -53,7 +62,40 @@ public class App {
         } while (!end);
     }
 
-    private static void drawFish(int size) {
+    public static void drawFish(int size) {
+
+        String spacesOUT = "", spacesIN = "", stars1 = "*", stars2 = "*";
+
+        for (int i = 0; i < size; ++i) {
+            spacesOUT += " ";
+        }
+
+        for (int i = 0; i < 2 * size + 1; ++i) {
+
+            // For upper part
+            if (i < size) {
+                System.out.print(spacesOUT + stars1 + spacesIN + stars1 + spacesOUT
+                        + spacesOUT.substring(0, spacesOUT.length() - 1) + stars1.repeat(i));
+                System.out.println();
+                spacesOUT = spacesOUT.substring(0, spacesOUT.length() - 1);
+                spacesIN += "  ";
+
+            }
+
+            // For middle part
+            if (i == size) {
+                System.out.println(spacesOUT + stars1 + spacesIN + stars2.repeat(size));
+            }
+
+            // For lower part
+
+            if (i > size) {
+                spacesOUT += " ";
+                spacesIN = spacesIN.substring(0, spacesIN.length() - 2);
+                System.out.println(spacesOUT + stars1 + spacesIN + stars1);
+
+            }
+        }
 
     }
 
