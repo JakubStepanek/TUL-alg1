0e2a10cb5e1dc455d70cf67f428a5820dfa54ed2
JakubStepanek <jakub.stepanek@tul.cz>
2021-12-15 21:31:53 +0100

    Vánoční úkol hotov

--- Alg_1_Project/src/App.java
+++ Alg_1_Project/src/App.java
@@ -1,14 +1,16 @@
 
+import java.io.Console;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+import javax.swing.plaf.synth.SynthSplitPaneUI;
 
 public class App {
     static public Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         showMenu();
-        drawFish(findDate());
-
-        // test();
     }
 
     private static void test() {
@@ -24,42 +26,40 @@ public class App {
         System.out.println("Pick one: ");
         System.out.println("1. Christmass event");
         System.out.println("Push ENTER for exit");
+        System.out.print("Option: ");
         String input = sc.nextLine();
         switch (input) {
             case "1":
-                christmasEvent();
+                chooseFish(christmasEvent(), findDate());
             case "2":
-                // ToDo
+                // SEMINARKA
                 break;
 
             default:
                 break;
         }
-
     }
 
     public static boolean christmasEvent() {
-        System.out.println("Christmas time began.");
+        System.out.println("Christmas time has begun.");
         int size;
         boolean eatsFish = false;
         boolean end = false;
-
+        System.out.println("I would like to ask you, do you like FISH for Christmas Eve? [yes/no]");
         do {
-            System.out.println("I would like to ask you, do you like FISH for Christmas Eve");
+            System.out.print("Answer: ");
             String answer = sc.nextLine().toLowerCase();
             switch (answer) {
                 case "yes":
                     eatsFish = true;
-                    System.out.println("nice");
                     end = true;
                     break;
                 case "no":
                     eatsFish = false;
-                    System.out.println("lucky you");
                     end = true;
                     break;
                 default:
-                    System.out.println("Wrong choice, try it again.");
+                    System.out.println(ConsoleColors.RED_BOLD + "Wrong choice, try it again." + ConsoleColors.RESET);
                     break;
             }
         } while (!end);
@@ -70,71 +70,98 @@ public class App {
     public static int findDate() {
         int size = 0;
         boolean end = false;
+        String date = "";
+        System.out.println(ConsoleColors.GREEN + "Thank You for answering. \n" + ConsoleColors.RESET
+                + "Now I would like to ask you what date is today? [1-31]");
         do {
-            System.out.println("Thank You for answering. Now I would like to ask you what date is today?");
-            int date = sc.nextInt();
+            System.out.print("Date: ");
+            date = sc.nextLine();
             switch (date) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                    size = 4;
+                case "1":
+                case "2":
+                case "3":
+                    size = 3;
                     end = true;
                     break;
-                case 11:
-                case 12:
-                case 13:
-                case 14:
-                case 15:
-                case 16:
-                case 17:
-                case 18:
-                case 19:
-                case 20:
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                    size = 5;
+                    end = true;
+                    break;
+                case "9":
+                case "10":
+                    size = 6;
+                    end = true;
+                    break;
+                case "11":
+                case "12":
+                case "13":
+                case "14":
+                case "15":
                     size = 8;
                     end = true;
                     break;
-                case 21:
-                case 22:
-                case 23:
-                case 24:
-                case 25:
-                case 26:
-                case 27:
-                case 28:
-                case 29:
-                case 30:
-                case 31:
+                case "16":
+                case "17":
+                case "18":
+                case "19":
+                case "20":
+                    size = 10;
+                    end = true;
+                    break;
+                case "21":
+                case "22":
+                case "23":
                     size = 12;
                     end = true;
                     break;
-                default:
-                    System.out.println("Wrong choice, try it again.");
+                case "24":
+                    size = 15;
                     end = true;
+                    break;
+                case "25":
+                    size = 17;
+                    end = true;
+                    break;
+                case "26":
+                case "27":
+                case "28":
+                case "29":
+                case "30":
+                case "31":
+                    size = 20;
+                    end = true;
+                    break;
+                default:
+                    System.out.println(ConsoleColors.RED_BOLD + "Wrong choice, try it again." + ConsoleColors.RESET);
                     break;
             }
         } while (!end);
-
         return (size);
     }
 
-    public static void drawFish(int size) {
+    public static void chooseFish(boolean eatsFish, int size) {
+        if (eatsFish) {
+            if (size <= 12) {
+                drawFish(size);
+            } else {
+                drawDeadFish();
+            }
+        } else {
+            drawFish(size);
+        }
+    }
 
+    public static void drawFish(int size) {
         String spacesOUT = "", spacesIN = "", spacesINtemp = "", spacesTailLOVER = "", stars1 = "*", stars2 = "*";
         int tailSize = size - 1;
-
         for (int i = 0; i < size; ++i) {
             spacesOUT += " ";
         }
-
         for (int i = 0; i < 2 * size + 1; ++i) {
-
             // For upper part
             if (i < size) {
                 if (i == size / 2 + 1) {
@@ -146,9 +173,7 @@ public class App {
                     spacesOUT = spacesOUT.substring(0, spacesOUT.length() - 1);
                     spacesIN += "  ";
                     spacesIN = spacesINtemp += "  ";
-
                 } else {
-
                     System.out.print(spacesOUT + stars1 + spacesIN + stars1 + spacesOUT
                             + spacesOUT.substring(0, spacesOUT.length() - 1) + stars1.repeat(i));
                     System.out.println();
@@ -156,16 +181,13 @@ public class App {
                     spacesIN += "  ";
                 }
             }
-
             // For middle part
             if (i == size) {
                 System.out.println(spacesOUT + spacesIN.substring(0, 1) + "==" + spacesIN.substring(2)
                         + stars2.repeat(size));
                 spacesOUT += " ";
             }
-
             // For lower part
-
             if (i > size) {
                 spacesTailLOVER += " ";
                 spacesIN = spacesIN.substring(0, spacesIN.length() - 2);
@@ -174,7 +196,6 @@ public class App {
                         + stars1.repeat((tailSize)));
                 spacesOUT += " ";
                 tailSize--;
-
             }
         }
         printText();
@@ -189,12 +210,13 @@ public class App {
         System.out.println("|  /    / / / / / / /   |__\\");
         System.out.println("|/     / / / / / / /");
         System.out.println();
+        printText();
     }
 
     public static void printText() {
         System.out.println();
         System.out.println(ConsoleColors.RED_BOLD + ConsoleColors.GREEN_BACKGROUND
-                + "Merry Christmas & Happy New Year. \nSending wishes your way for a great holiday season and a super fabulous year ahead!"
+                + "Merry Christmas & Happy New Year.\n Sending wishes your way for a great holiday season and a super fabulous year ahead!"
                 + ConsoleColors.RESET);
         System.out.println();
     }
