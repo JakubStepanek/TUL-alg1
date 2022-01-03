f44de6022f31acc95c334bc15593407133d47680
JakubStepanek <jakub.stepanek@tul.cz>
2022-01-03 12:33:13 +0100

    HOTOTO

--- Alg_1_Project/src/App.java
+++ Alg_1_Project/src/App.java
@@ -5,14 +5,17 @@ public class App {
         showMenu();
         Scanner sc = new Scanner(System.in);
         boolean end = false;
-        String key = sc.nextLine().toLowerCase();
+        String key;
         do {
+        key = sc.nextLine().toLowerCase();
             switch (key) {
                 case "1":
                     DrawFish.main(args);
+                    showMenu();
                     break;
                 case "2":
                     FindPatternInSequence.main(args);
+                    showMenu();
                     break;
                 case "0":
                     System.out.println("Program se ukoncuje...");
